1df56edbf89b5667bc96b9f997634f40423da49e
JOY0919 <zixu0859@uni.sydney.edu.au>
2024-11-01 21:51:38 +1100

    Update sketch.js
    Yellow background line  01

--- major project/sketch.js
+++ major project/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
+    background(255);
+  
+  // Set the stroke color and weight for the yellow lines
+  stroke(225,225.0);
+  strokeWeight( 10 );
+  
+  let y = 0;
+  for (let i = 0; i < 5; i++){
+    line(0, y, width,y);
+    y += random(50,80);
+  }
+  
+  let x = 0;
+  for (let j =0; j < 8; j++) {
+    line(x, 0, x, height);
+    x +=random(30,60) ;
+  }
 }
