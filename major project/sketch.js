85dc076c5f23432195783e6ba18f8d6db9ad47ae
zzha0024 <zzha0024@uni.sydney.edu.au>
2024-11-04 22:35:04 +0800

    Modified previous version(random rects)
    This has been modified so that random rectangles do not overlay fixed rectangles.

--- major project/sketch.js
+++ major project/sketch.js
@@ -24,14 +24,18 @@ function setup() {
 
   for (let x of xPositions ){
       line(x, 0, x, height);
-  
   }
 
 
 //Zichen Zhang
 //Generate red, blue and gray squares that appear in different positions each time they refresh
-
 //Some fixed squares, representing objects（such as some buildings) that don't change
+
+drawfixedRects();
+randomRect();
+}
+
+function drawfixedRects(){
 fill('red');
 rect(30,60,50,80);
 
@@ -43,32 +47,68 @@ rect (45,290,80,50);
 
 fill('red');
 rect(280,280,70,90);
-
-
-//Random squares, representing objects that change over time
-
-randomRect();
 }
 
+//Random rects, representing objects that change over time
 
 function randomRect(){
   let colors = ['red','grey','blue'];
 
+  //Fixed the size and location of rects.
+  let fixedRects = [ 
+    { x:30, y:60, w:50, h:80},
+    { x:250, y:60, w:50, h:80},
+    { x:45, y:290, w:80, h:50},
+    { x:280, y:280, w:70, h:90},
+  ];
+
   for (let i=0; i< 5; i++) {
-    let x = random(0, 350);
-    let y = random(0, 350);
-    let rectSize = random(20,100);
+    let rectSize = random(20,80);
+    let x, y;
+    let overlapping = true;
+    
+//while loops are helpful for repeating statements while a condition is true. They're like if statements that repeat.
+//Loop through the locations until find one that doesn't overlap
+while (overlapping){
+  x = random(0, 330);
+  y = random(0, 330);
+  overlapping = false;
 
+//Check to see if random rects overlaps the fixed rects
+  for (let rect of fixedRects){
+    let horizontalOverlap = false;
+    let verticalOverlap = false;
+  
+//Determine whether there is horizontal overlap
+if (x < rect.x + rect.w) {
+  if (x + rectSize > rect.x){
+    horizontalOverlap = true;
+  }
+}
+//Determine whether there is vertical overlap
+if (y < rect.y + rect.h){
+  if (y + rectSize > rect.y){
+    verticalOverlap = true;
+  }
+}
 
+//If both directions have overlapping, set overlapping to true
+if (horizontalOverlap){
+  if (verticalOverlap){
+    overlapping = true;
+    break;
+     }
+    }
+   }
+  }
 
     let color = random(colors);
     fill(color);
     noStroke();
     rect(x,y,rectSize, rectSize);
-
-
+   }
   }
-}
+
 
 
 
